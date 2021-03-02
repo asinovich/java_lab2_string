abc4ffe0eb6da55915cecf0f025bbeb0907b912f
Sergeu <asinovich2015@mail.ru>
2021-03-02 16:59:23 +0300

    init lab2(string)

---
+++ src/main/java/Action_on_string.java
@@ -0,0 +1,18 @@
+public class Action_on_string {
+
+    public static String low(String str){
+        return str.toLowerCase();
+    }
+
+    public static String up(String str){
+        return str.toUpperCase();
+    }
+
+    public static int size(String str){
+        return str.length();
+    }
+
+    public static String unite(String s1, String s2){
+        return (s1 + s2);
+    }
+}
